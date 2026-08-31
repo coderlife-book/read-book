7e65ab302beb3ad12b4bc48f4e99ccbef98fe306
Su Chen <supercoolcoder@qq.com>
2026-08-31 13:55:47 +0800

    chore: bump app bundle to v0.1.2

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -24,8 +24,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key><string>com.coderlife.readbook</string>
   <key>CFBundleName</key><string>ReadBook</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>0.1.1</string>
-  <key>CFBundleVersion</key><string>2</string>
+  <key>CFBundleShortVersionString</key><string>0.1.2</string>
+  <key>CFBundleVersion</key><string>3</string>
   <key>LSMinimumSystemVersion</key><string>26.0</string>
   <key>NSHighResolutionCapable</key><true/>
 </dict>
