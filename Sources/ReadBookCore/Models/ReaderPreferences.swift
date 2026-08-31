2881af8a22c143e5219d5aa76a97f6de59dbbef8
Su Chen <supercoolcoder@qq.com>
2026-08-31 14:39:26 +0800

    feat: add stealth reader preferences

--- Sources/ReadBookCore/Models/ReaderPreferences.swift
+++ Sources/ReadBookCore/Models/ReaderPreferences.swift
@@ -3,6 +3,8 @@ import Foundation
 public enum ReadingMode: String, Codable, Sendable { case paginated, continuous }
 public enum ReaderTheme: String, Codable, Sendable { case soft, light, dark }
 public enum AppPresenceMode: String, Codable, Sendable { case widgetStyle, normal }
+public enum BossModeProfile: String, Codable, Sendable { case floatingReading, concealed }
+public enum ReaderWindowAppearance: String, Codable, Sendable { case card, frameless, transparent }
 
 public struct ReaderPreferences: Codable, Equatable, Sendable {
     public var readingMode: ReadingMode
@@ -13,6 +15,10 @@ public struct ReaderPreferences: Codable, Equatable, Sendable {
     public var theme: ReaderTheme
     public var alwaysOnTop: Bool
     public var appPresenceMode: AppPresenceMode
+    public var bossModeEnabled: Bool
+    public var bossModeProfile: BossModeProfile
+    public var windowAppearance: ReaderWindowAppearance
+    public var framelessBackgroundOpacity: Double
 
     public init(
         readingMode: ReadingMode,
@@ -22,7 +28,11 @@ public struct ReaderPreferences: Codable, Equatable, Sendable {
         paragraphSpacing: Double,
         theme: ReaderTheme,
         alwaysOnTop: Bool,
-        appPresenceMode: AppPresenceMode
+        appPresenceMode: AppPresenceMode,
+        bossModeEnabled: Bool = false,
+        bossModeProfile: BossModeProfile = .floatingReading,
+        windowAppearance: ReaderWindowAppearance = .card,
+        framelessBackgroundOpacity: Double = 0.18
     ) {
         self.readingMode = readingMode
         self.fontFamily = fontFamily
@@ -32,6 +42,63 @@ public struct ReaderPreferences: Codable, Equatable, Sendable {
         self.theme = theme
         self.alwaysOnTop = alwaysOnTop
         self.appPresenceMode = appPresenceMode
+        self.bossModeEnabled = bossModeEnabled
+        self.bossModeProfile = bossModeProfile
+        self.windowAppearance = windowAppearance
+        self.framelessBackgroundOpacity = Self.clampFramelessOpacity(framelessBackgroundOpacity)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case readingMode
+        case fontFamily
+        case fontSize
+        case lineSpacing
+        case paragraphSpacing
+        case theme
+        case alwaysOnTop
+        case appPresenceMode
+        case bossModeEnabled
+        case bossModeProfile
+        case windowAppearance
+        case framelessBackgroundOpacity
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        readingMode = try container.decode(ReadingMode.self, forKey: .readingMode)
+        fontFamily = try container.decode(String.self, forKey: .fontFamily)
+        fontSize = try container.decode(Double.self, forKey: .fontSize)
+        lineSpacing = try container.decode(Double.self, forKey: .lineSpacing)
+        paragraphSpacing = try container.decode(Double.self, forKey: .paragraphSpacing)
+        theme = try container.decode(ReaderTheme.self, forKey: .theme)
+        alwaysOnTop = try container.decode(Bool.self, forKey: .alwaysOnTop)
+        appPresenceMode = try container.decode(AppPresenceMode.self, forKey: .appPresenceMode)
+        bossModeEnabled = try container.decodeIfPresent(Bool.self, forKey: .bossModeEnabled) ?? false
+        bossModeProfile = try container.decodeIfPresent(BossModeProfile.self, forKey: .bossModeProfile) ?? .floatingReading
+        windowAppearance = try container.decodeIfPresent(ReaderWindowAppearance.self, forKey: .windowAppearance) ?? .card
+        framelessBackgroundOpacity = Self.clampFramelessOpacity(
+            try container.decodeIfPresent(Double.self, forKey: .framelessBackgroundOpacity) ?? 0.18
+        )
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(readingMode, forKey: .readingMode)
+        try container.encode(fontFamily, forKey: .fontFamily)
+        try container.encode(fontSize, forKey: .fontSize)
+        try container.encode(lineSpacing, forKey: .lineSpacing)
+        try container.encode(paragraphSpacing, forKey: .paragraphSpacing)
+        try container.encode(theme, forKey: .theme)
+        try container.encode(alwaysOnTop, forKey: .alwaysOnTop)
+        try container.encode(appPresenceMode, forKey: .appPresenceMode)
+        try container.encode(bossModeEnabled, forKey: .bossModeEnabled)
+        try container.encode(bossModeProfile, forKey: .bossModeProfile)
+        try container.encode(windowAppearance, forKey: .windowAppearance)
+        try container.encode(framelessBackgroundOpacity, forKey: .framelessBackgroundOpacity)
+    }
+
+    private static func clampFramelessOpacity(_ value: Double) -> Double {
+        min(max(value, 0), 0.60)
     }
 
     public static let defaults = ReaderPreferences(
@@ -42,6 +109,10 @@ public struct ReaderPreferences: Codable, Equatable, Sendable {
         paragraphSpacing: 9,
         theme: .soft,
         alwaysOnTop: false,
-        appPresenceMode: .widgetStyle
+        appPresenceMode: .widgetStyle,
+        bossModeEnabled: false,
+        bossModeProfile: .floatingReading,
+        windowAppearance: .card,
+        framelessBackgroundOpacity: 0.18
     )
 }
