ff0f6aaeeef02dfd1980626bab1bba9039b6dee4
Su Chen <supercoolcoder@qq.com>
2026-08-31 15:08:49 +0800

    feat: clean up global input on termination

--- Sources/ReadBook/App/AppDelegate.swift
+++ Sources/ReadBook/App/AppDelegate.swift
@@ -3,15 +3,18 @@ import AppKit
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
     var flushHandler: (@MainActor () async -> Void)?
+    var cleanupHandler: (@MainActor () -> Void)?
     private var terminationPending = false
 
     func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
         guard !terminationPending, let flushHandler else {
+            cleanupHandler?()
             return terminationPending ? .terminateLater : .terminateNow
         }
 
         terminationPending = true
         Task { @MainActor in
+            cleanupHandler?()
             await flushHandler()
             sender.reply(toApplicationShouldTerminate: true)
         }
